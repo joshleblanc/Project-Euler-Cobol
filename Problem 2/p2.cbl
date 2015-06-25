5fa9cc1226eab1b1393c116fae2613ba1548a0c7
HorizonShadow <joshleblanc94@gmail.com>
2015-06-25 09:15:04 -0300

    p2 idnentation fix

--- Problem 2/p2.cbl
+++ Problem 2/p2.cbl
@@ -1,20 +1,20 @@
         IDENTIFICATION DIVISION.
         PROGRAM-ID. HELLO-WORLD.
         DATA DIVISION.
-        	WORKING-STORAGE SECTION.
-        	01	SUM	PIC 9999999 VALUE 0.
-        	01	LOOP.
-        		02 FIRS PIC 9999999 VALUE 1.
-        		02 SECO PIC 9999999 VALUE 2.
+            WORKING-STORAGE SECTION.
+            01  SUM   PIC 9999999 VALUE 0.
+            01  LOOP.
+            02 FIRS PIC 9999999 VALUE 1.
+            02 SECO PIC 9999999 VALUE 2.
                 02 REM PIC 9999999.
                 02 TMP PIC 9999999.
-        		02 ANS PIC 9999999.
+            02 ANS PIC 9999999.
         PROCEDURE DIVISION.
         MAIN-PARA.
-        	PERFORM MAIN-LOOP UNTIL ANS >= 4000000.
+            PERFORM MAIN-LOOP UNTIL ANS >= 4000000.
             ADD 2 TO SUM.
             DISPLAY SUM.
-        	ACCEPT SUM.
+            ACCEPT SUM.
             STOP RUN.
         MAIN-LOOP.
             SET ANS TO FIRS.
