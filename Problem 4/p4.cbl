4c927531cd364d4b67b863beb087182adb3a39b0
HorizonShadow <joshleblanc94@gmail.com>
2015-06-25 08:51:07 -0300

    p4 indentation fix

--- Problem 4/p4.cbl
+++ Problem 4/p4.cbl
@@ -1,25 +1,25 @@
         IDENTIFICATION DIVISION.
         PROGRAM-ID. HELLO-WORLD.
         DATA DIVISION.
-        	WORKING-STORAGE SECTION.
+            WORKING-STORAGE SECTION.
             01	SUM	PIC 999999.
             01  CURRNUM PIC 999999999999 VALUE 600851475143.
-            01 MAX PIC 999999 VALUE 0.
-        	01	LOOP.
-        		02 I PIC 999 VALUE 999.
+            01  MAX PIC 999999 VALUE 0.
+            01  LOOP.
+                02  I PIC 999 VALUE 999.
             01  ILOOP.
-                02 TMP PIC 999999.
-                02 S PIC X(6).
-                02 RS PIC X(6).
-                02 J PIC 999 VALUE 999.
+                02  TMP PIC 999999.
+                02  S PIC X(6).
+                02  RS PIC X(6).
+                02  J PIC 999 VALUE 999.
         PROCEDURE DIVISION.
         MAIN-PARA.
-        	PERFORM MAIN-LOOP UNTIL I = 0.
+            PERFORM MAIN-LOOP UNTIL I = 0. 
             DISPLAY MAX.
             ACCEPT SUM.
             STOP RUN.
         MAIN-LOOP.
-        	PERFORM INNER-LOOP UNTIL J = 0.
+            PERFORM INNER-LOOP UNTIL J = 0.
             SUBTRACT 1 FROM I.
             SET J TO I.
         INNER-LOOP
